d75d81248b27f185d23aa616f9aff21b9d838797
BlueSteel <bluesteel@BlueSteel.local>
2019-10-11 08:29:03 -0500

    added header component and liks

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import HomePage from './pages/homepage/homepage.component';
 
 import ShopPage from './pages/shop/shop.component';
 
+import Header from './components/header/header.component.jsx'
+
 import './App.css';
 
 const HatsPage = () => (
@@ -16,6 +18,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+    <Header/>
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route exact path='/shop/hats' component={HatsPage} />
